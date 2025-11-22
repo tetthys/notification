0043a40ef339013f2e57d39ef3d7b1bbf82810ba
= <=>
2025-11-22 16:32:23 +0900

    add phpDoc

--- src/Core/Model/Notification.php
+++ src/Core/Model/Notification.php
@@ -4,6 +4,23 @@ namespace Tetthys\Notification\Core\Model;
 
 final class Notification
 {
+    /**
+     * Constructs a new Notification instance.
+     * 
+     * @param string $id The unique identifier for the notification.
+     * @param string $source The source of the notification.
+     * @param string $type The type/category of the notification.
+     * @param array $recipients List of recipients for the notification.
+     * @param array $channels Channels through which the notification will be sent.
+     * @param array $content Rendered content for each channel.
+     * @param int $priority Priority level of the notification.
+     * @param \DateTimeImmutable $timestamp Timestamp when the notification was created.
+     * @param string $status Current status of the notification.
+     * @param string|null $parentId Optional parent notification ID.
+     * @param string|null $tenantId Optional tenant ID for multi-tenant systems.
+     * 
+     * @return void
+     */
     public function __construct(
         public string $id,
         public string $source,
